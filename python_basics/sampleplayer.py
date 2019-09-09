68f82427b01b0a8c24c31ee9bc5a093ff1e12cd9
Ruben Middelman <rubenmiddelman@gmail.com>
2019-09-09 13:18:10 +0200

    Adds the sampleplayer.py which is a program that lets you choose the amount of samples that are played

--- python_basics/sampleplayer.py
+++ python_basics/sampleplayer.py
@@ -1,2 +1,17 @@
-from playsound import playsound
-playsound('a-little-bit-drunk_84bpm_A_minor.wav')
+import simpleaudio as sa
+
+global numOfPlays
+numOfPlays = 0
+if numOfPlays < 1:
+    numOfPlays = int(input("How many times doy you want to hear the sample  "))
+
+def PlaySample():
+    wave_obj = sa.WaveObject.from_wave_file("lo-fi-snare_B_minor.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+while numOfPlays > 0:
+    PlaySample()
+    numOfPlays = numOfPlays  - 1
+
+##makes a function that plays the sample
